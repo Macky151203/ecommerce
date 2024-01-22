38b9efe65e1040a739e645736fa7e41c7c13d187
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2024-01-22 19:23:22 +0530

    frontend modifications

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,42 +2,7 @@
 import {useState} from 'react';
 function Navbar(){
     const [menubar, setMenubar]=useState(false)
-    const handleEnter = (e: { target: { className: string; }; }) => {
-        e.target.className="active"
-    }
-    const handleLeave = (e: { target: { className: string; }; }) => {
-        e.target.className="navbar-link"
-    }
-    const openMenubar = (e: any) => {
-        if(!menubar){
-            const element =document.getElementById("navbar-links");
-            element.style.display="block";
-            element.style.position="absolute";
-            element.style.top="10vh";
-            element.style.zIndex="20";
-            element.style.width="100%";
-            const bars=document.getElementsByClassName("bar");
-            for(var i=0;i<bars.length;i++){
-                bars[i].style.backgroundColor="#d2d7d3"
-            }
-            element.style.backgroundColor="white";
-            
-            const navbar=document.getElementsByClassName("navbar")[0];
-            navbar.style.backgroundColor="white";
-        }
-        else{
-            const element =document.getElementById("navbar-links");
-            element.style.display="none";
-            const bars=document.getElementsByClassName("bar");
-            for(var i=0;i<bars.length;i++){
-                bars[i].style.backgroundColor="white"
-            }
-            
-            const navbar=document.getElementsByClassName("navbar")[0];
-            navbar.style.background="transparent";
-        }
-        setMenubar(!menubar)
-    }
+    
     return(
         <nav className="navbar w-full flex justify-around items-center py-6 relative top-0 right-0 left-0 z-10">
             <div className="navbar-brand w-2/6">
@@ -46,7 +11,7 @@ function Navbar(){
                     <p className="text-black mx-auto text-center">MEN'S FASHION</p>
                 </div>
             </div>
-            <div className="block w-1/2 md:hidden menu-bar" onClick={openMenubar}>
+            <div className="block w-1/2 md:hidden menu-bar">
                 <div className="w-3/12 sm:w-1/5 mx-auto h-1/2 border-solid border-2 border-white p-2 rounded-lg">
                     {
                         (menubar)?
@@ -65,11 +30,11 @@ function Navbar(){
             </div>
             <div className="navbar-links hidden md:block text-[#1e1e1ebb] w-1/2 text-md my-4" id="navbar-links">
                 <ul className="list-none block md:flex md:justify-evenly md:gap-y-1.5 text-center">
-                    <li onMouseEnter={handleEnter} onMouseLeave={handleLeave}>About</li>
-                    <li onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Products</li>
-                    <li onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Contacts</li>
-                    <li onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Login</li>
-                    <li onMouseEnter={handleEnter} onMouseLeave={handleLeave}>SignUp</li>
+                    <li>About</li>
+                    <li>Products</li>
+                    <li>Contacts</li>
+                    <li>Login</li>
+                    <li>SignUp</li>
                 </ul>
             </div>
         </nav>
